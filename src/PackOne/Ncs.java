78a158afe96d03ac98e92921896093460a997cfa
Yaskovskiy Ivan <harveylynx@yandex.ru>
2017-02-17 11:39:29 +0200

    Initial commit

---
+++ src/PackOne/Ncs.java
@@ -0,0 +1,13 @@
+package PackOne;
+public class Ncs {
+    public static void main(String[] args){
+System.out.println("HL");
+        Object ass = new Object();
+        for (int i = 0; i < 20; i++) {
+            if(i%2==0){
+                System.out.println("i = " + i);
+            }
+        }
+    }
+}
+
